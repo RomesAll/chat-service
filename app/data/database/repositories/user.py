5a80bffb7189032ac5b7fea46bd8ac7b3ae455f2
RomesAll <romesky845@gmail.com>
2026-08-04 15:48:28 +0300

    feat: добавил репозиторий для пользователей

---
+++ app/data/database/repositories/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Session
+from app.services.dtos.user import UserDtoGetResponse
+from models.user import UserOrm
+from .base import BaseRepository
+
+
+class UserRepository(BaseRepository):
+    """Репозиторий для работы с данными пользователей"""
+    def __init__(self, session: Session):
+        super().__init__(
+            dto_response=UserDtoGetResponse,
+            session=session,
+            model=UserOrm
+        )
