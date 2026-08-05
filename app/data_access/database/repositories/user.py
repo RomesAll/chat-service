5fc83502f2bb4e1abe0be7eab3039120e87e4734
RomesAll <romesky845@gmail.com>
2026-08-05 16:33:33 +0300

    feat: добавил преобразование выходных данных из базового репозитьория для репозитория управления пользователями чтобы pycharm и линтер не ругался на типы возв. знач. в методах

--- app/data_access/database/repositories/user.py
+++ app/data_access/database/repositories/user.py
@@ -1,6 +1,9 @@
+from typing import cast
 from sqlalchemy.orm import Session
+from app.shared.dtos.base import DtoIdRecordRequest
 from app.shared.dtos.user import UserDtoGetResponse
 from models.user import UserOrm
+from shared.dtos.base import BaseDtoGetListRequest, BaseDtoGetResponse
 from .base import BaseRepository
 
 
@@ -12,3 +15,19 @@ class UserRepository(BaseRepository):
             session=session,
             model=UserOrm
         )
+
+    def get(self, dto_get_request: BaseDtoGetListRequest) -> list[UserDtoGetResponse]:
+        """
+        Получение списка пользователей
+        :param dto_get_request:
+        :return:
+        """
+        return cast(list[UserDtoGetResponse], super().get(dto_get_request))
+
+    def get_by_id(self, dto_record_id: DtoIdRecordRequest) -> UserDtoGetResponse:
+        """
+        Получение пользователей по id
+        :param dto_record_id:
+        :return:
+        """
+        return cast(UserDtoGetResponse, super().get_by_id(dto_record_id))
