29bb9c743819b3357cafc6098f70e59a1c6ec3c2
RomesAll <romesky845@gmail.com>
2026-08-08 13:40:47 +0300

    refactor: обновил базовый repr метод для всех orm моделей

--- app/data_access/database/models/base.py
+++ app/data_access/database/models/base.py
@@ -43,17 +43,23 @@ class BaseOrm(DeclarativeBase, IdMixin, TimeStampMixin, metaclass=OrmManagerMeta
         }
 
     def soft_delete(self) -> bool:
-        """Мягкое удаление"""
+        """Метод мягкого удаления объекта, делает его не активным"""
         self.is_deleted = True
         return bool(self.is_deleted)
 
     def soft_recovery(self) -> bool:
-        """Мягкое восстановление"""
+        """Восстановление неактивного объекта"""
         self.is_deleted = False
         return bool(self.is_deleted)
-
 
     @classmethod
     def get_all_models(cls):
         """Получить все зарегистрированные модели (доступно через экземпляр класса)"""
-        return cls.__class__.get_all_models()
+        return OrmManagerMeta.get_all_models()
+
+    def __repr__(self):
+        return (f'<{self.__class__.__name__}('
+                f'id={self.id}, '
+                f'is_deleted={self.is_deleted}, '
+                f'created_at={self.created_at.strftime('%d.%m.%Y %H:%M') if self.created_at else self.created_at}, '
+                f'updated_at={self.updated_at.strftime('%d.%m.%Y %H:%M') if self.updated_at else self.updated_at})>')
