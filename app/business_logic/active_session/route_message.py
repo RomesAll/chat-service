fbcb9a3e0c74695638de869925319832db4907d0
RomesAll <romesky845@gmail.com>
2026-08-12 01:11:50 +0300

    feat: добавил создание private_message_route и group_message_route внутри класса

--- app/business_logic/active_session/route_message.py
+++ app/business_logic/active_session/route_message.py
@@ -1,9 +1,10 @@
+from business_logic.active_session.active_session_manager import ActiveSessionManager
 from business_logic.active_session.message_sender.group_message import GroupMessageRoute
 from business_logic.active_session.message_sender.interface import IMessageRoute
 from business_logic.active_session.message_sender.private_message import PrivateMessageRoute
 from business_logic.exceptions import RouteMessageError
 from models.message import MessageType
-from shared.dtos.message import BaseMessageDtoSend
+from app.shared.dtos import BaseMessageDtoPostRequest
 
 
 class RouteMessage:
@@ -11,12 +12,13 @@ class RouteMessage:
 
     def __init__(
             self,
-            private_message_route: PrivateMessageRoute,
-            group_message_route: GroupMessageRoute
+            active_session: ActiveSessionManager,
     ):
+        # Менеджер для хранения и управления активными сессиями
+        self.active_session = active_session
         # Приватные и групповые пути для отправки сообщений
-        self.private_message_route = private_message_route
-        self.group_message_route = group_message_route
+        self.private_message_route = PrivateMessageRoute(active_session)
+        self.group_message_route = GroupMessageRoute(active_session)
         # Маппинг сообщений по типам сообщения
         self.MAPPING_MESSAGE_TYPE = {
             MessageType.PRIVATE: self.private_message_route,
@@ -25,10 +27,10 @@ class RouteMessage:
 
     async def routing_message(
             self,
-            message_request: BaseMessageDtoSend,
+            message_dto_request: BaseMessageDtoPostRequest,
     ):
         """Маршрутизация сообщений"""
-        service: IMessageRoute | None = self.MAPPING_MESSAGE_TYPE.get(message_request.type)
-        if not service:
-            raise RouteMessageError(message_request)
-        await service.send_message(message_request)
+        route: IMessageRoute | None = self.MAPPING_MESSAGE_TYPE.get(message_dto_request.type)
+        if not route:
+            raise RouteMessageError(message_dto_request)
+        await route.send_message(message_dto_request)
