847aeceb6b2ac0ea9f1bd401dd5f4f0c331d69dd
RomesAll <romesky845@gmail.com>
2026-08-12 01:12:18 +0300

    feat: обновил отправку сообщейни в обе стороны

--- app/business_logic/active_session/message_sender/private_message.py
+++ app/business_logic/active_session/message_sender/private_message.py
@@ -1,19 +1,42 @@
+from business_logic.active_session.active_session_manager import ActiveSessionManager
 from business_logic.active_session.message_sender.interface import IMessageRoute
 from business_logic.exceptions import SendMessageError
-from shared.dtos.message import PrivateMessageDtoSend
+from app.shared.dtos import PrivateMessageDtoPostRequest, ActiveSession
 
 
 class PrivateMessageRoute(IMessageRoute):
     """Сервис для управления отправкой приватных сообщений"""
 
+    def __init__(
+            self,
+            active_session: ActiveSessionManager
+    ):
+        # Менеджер для хранения и управления активными сессиями
+        self.active_session = active_session
+
     async def send_message(
             self,
-            message_send_request: PrivateMessageDtoSend
+            message_send_request: PrivateMessageDtoPostRequest
     ):
         """Отправка сообщения пользователю"""
         try:
-            for websocket in message_send_request.connections:
-                await websocket.send_json(message_send_request)
+            sender_active_session: ActiveSession = self.active_session.get_or_create_session(
+                user_id=message_send_request.sender_id
+            )
+            target_active_session: ActiveSession = self.active_session.get_or_create_session(
+                user_id=message_send_request.recipient_id
+            )
+            if not sender_active_session.websockets:
+                raise Exception
+            if not target_active_session.websockets:
+                raise Exception
+            for sender_connection in sender_active_session.websockets:
+                sender_json_data = message_send_request.model_dump_json()
+                await sender_connection.send_json(sender_json_data)
+            if message_send_request.sender_id != message_send_request.recipient_id:
+                for target_connection in target_active_session.websockets:
+                    target_json_data = message_send_request.model_dump_json()
+                    await target_connection.send_json(target_json_data)
         except Exception as e:
             raise SendMessageError(
                 message_send_request.sender_id,
