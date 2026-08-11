89885def997de8ea48ce21710c74c5b07192588e
RomesAll <romesky845@gmail.com>
2026-08-12 00:53:32 +0300

    refactor: поменял название для сообщений

--- app/shared/dtos/message.py
+++ app/shared/dtos/message.py
@@ -1,13 +1,10 @@
 from enum import Enum
 from uuid import UUID
-from pydantic import ConfigDict
-from starlette.websockets import WebSocket
-
+from pydantic import Field
 from .base import (
-    BaseDtoOrmRecordGetResponse,
-    BaseDtoOrmRecordPostRequest,
-    BaseDtoOrmRecordPutResponse,
-    BaseDtoOrmRecordDeleteResponse
+    BaseDtoGetResponse,
+    BaseDtoPostDeleteRequest,
+    BaseDtoPutPathRequest,
 )
 
 
@@ -17,7 +14,7 @@ class MessageType(str, Enum):
     GROUP = 'group'
 
 
-class BaseMessageDtoGetResponse(BaseDtoOrmRecordGetResponse):
+class BaseMessageDtoGetResponse(BaseDtoGetResponse):
     """Базовый Message DTO для операции получения (Get) информации о сообщении"""
     sender_id: UUID
     message: str
@@ -27,14 +24,16 @@ class BaseMessageDtoGetResponse(BaseDtoOrmRecordGetResponse):
 class PrivateMessageDtoGetResponse(BaseMessageDtoGetResponse):
     """Private message_sender DTO для операции получения (Get) информации о приватных сообщениях"""
     recipient_id: UUID
+    type: MessageType = Field(MessageType.PRIVATE)
 
 
 class GroupMessageDtoGetResponse(BaseMessageDtoGetResponse):
     """Group message_sender DTO для операции получения (Get) информации о групповых сообщениях"""
     chat_id: UUID
+    type: MessageType = Field(MessageType.GROUP)
 
 
-class BaseMessageDtoPostRequest(BaseDtoOrmRecordPostRequest):
+class BaseMessageDtoPostRequest(BaseDtoPostDeleteRequest):
     """Базовый Message DTO для операции добавления (Post) информации о сообщении"""
     sender_id: UUID
     message: str
@@ -44,34 +43,21 @@ class BaseMessageDtoPostRequest(BaseDtoOrmRecordPostRequest):
 class PrivateMessageDtoPostRequest(BaseMessageDtoPostRequest):
     """Private message_sender DTO для операции добавления (Post) информации о приватных сообщениях"""
     recipient_id: UUID
+    type: MessageType = Field(MessageType.PRIVATE)
 
 
 class GroupMessageDtoPostRequest(BaseMessageDtoPostRequest):
     """Group message_sender DTO для операции добавления (Post) информации о групповых сообщениях"""
     chat_id: UUID
+    type: MessageType = Field(MessageType.GROUP)
 
 
-class MessageDtoUpdateRequest(BaseDtoOrmRecordPutResponse):
+class MessageDtoUpdateRequest(BaseDtoPutPathRequest):
     """Message DTO для операции обновления (Put) информации о сообщении"""
     message: str
     is_deleted: bool
 
 
-class MessageDtoDeleteRequest(BaseDtoOrmRecordDeleteResponse):
+class MessageDtoDeleteRequest(BaseDtoPostDeleteRequest):
     """Message DTO для операции удаления (Delete) информации о сообщении"""
     pass
-
-
-class BaseMessageDtoSend(BaseMessageDtoPostRequest):
-    connections: set[WebSocket]
-    model_config = ConfigDict(arbitrary_types_allowed=True)
-
-
-class GroupMessageDtoSend(BaseMessageDtoSend):
-    """Message DTO для broadcast сообщений"""
-    chat_id: UUID
-
-
-class PrivateMessageDtoSend(BaseMessageDtoSend):
-    """Message DTO для broadcast сообщений"""
-    recipient_id: UUID
