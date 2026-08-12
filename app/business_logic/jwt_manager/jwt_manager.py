76a33a16ae1800c60276a48fceda8a21cfc778a7
RomesAll <romesky845@gmail.com>
2026-08-12 17:32:34 +0300

    feat: добавил менеджер для создания и декодирования токенов доступа обновления

--- app/business_logic/jwt_manager/jwt_manager.py
+++ app/business_logic/jwt_manager/jwt_manager.py
@@ -1,24 +1,58 @@
+from abc import abstractmethod, ABC
+from copy import copy
+from datetime import timedelta, datetime, timezone
+from typing import TypeVar, Generic
+from app.shared.dtos import JWTAccessToken, JWTRefreshToken, JWTBaseToken
 import jwt
 
-class JwtManager:
-    """Менеджер для выпуска и декодирования токенов доступа, обновления"""
+T = TypeVar('T', bound=JWTBaseToken)
+
+class JWTBaseManager(ABC, Generic[T]):
+    """Базовый класс менеджер для выпуска и декодирования токенов доступа, обновления"""
+
+    SECRET_KEY: str = ""
+    ALGORITHM: str = "HS256"
+    EXPIRES_DELTA: timedelta = timedelta(minutes=15)
 
     @classmethod
-    def create_access_token(cls, data: dict) -> str:
-        """Создание токена доступа"""
-        pass
+    def create_token(cls, data: T) -> str:
+        """Создание токена доступа, обновления"""
+        to_encode: T = copy(data)
+        to_encode.exp = int((datetime.now(tz=timezone.utc) + cls.EXPIRES_DELTA).timestamp())
+        return jwt.encode(to_encode.model_dump(mode='json'), cls.SECRET_KEY, algorithm=cls.ALGORITHM)
 
     @classmethod
-    def create_refresh_token(cls, data: dict) -> str:
-        """Создание токена обновления"""
-        pass
+    def decode_token(cls, token: str) -> T:
+        """Декодирование токена доступа, обновления"""
+        to_decode = jwt.decode(token, cls.SECRET_KEY, algorithms=[cls.ALGORITHM,])
+        return cls._create_token_instance(to_decode_token=to_decode)
 
     @classmethod
-    def decode_access_token(cls, token: str) -> dict:
-        """Декодирование токена доступа"""
+    @abstractmethod
+    def _create_token_instance(cls, to_decode_token: dict) -> T:
+        """Создает экземпляр токена (должен быть переопределен)"""
         pass
 
+
+class JWTAccessManager(JWTBaseManager[JWTAccessToken]):
+    """Менеджер для выпуска и декодирования токенов доступа"""
+
+    SECRET_KEY: str = "access_secret_key_12345678901234567890"
+    EXPIRES_DELTA: timedelta = timedelta(minutes=15)
+
     @classmethod
-    def decode_refresh_token(cls, token: str) -> dict:
-        """Декодирование токена обновления"""
-        pass
+    def _create_token_instance(cls, to_decode_token: dict) -> JWTAccessToken:
+        """Создает экземпляр токена"""
+        return JWTAccessToken(**to_decode_token)
+
+
+class JWTRefreshManager(JWTBaseManager[JWTRefreshToken]):
+    """Менеджер для выпуска и декодирования токенов обновления"""
+
+    SECRET_KEY: str = "refresh_secret_key_12345678901234567890"
+    EXPIRES_DELTA: timedelta = timedelta(days=7)
+
+    @classmethod
+    def _create_token_instance(cls, to_decode_token: dict) -> JWTRefreshToken:
+        """Создает экземпляр токена"""
+        return JWTRefreshToken(**to_decode_token)
