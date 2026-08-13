d63fa0a2f04fadfcfc0accb30df037804aa27ed5
RomesAll <romesky845@gmail.com>
2026-08-13 17:32:08 +0300

    feat: добавил зависимости для проверки токенов и ролей

---
+++ app/presentation/dependencies/auth.py
@@ -0,0 +1,36 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from starlette import status
+from business_logic.auth.jwt_manager import JWTAccessManager
+from dtos import JWTAccessToken
+import jwt
+from models.user import RoleEnum
+
+security = HTTPBearer()
+
+
+class AuthChecker:
+    def __call__(self, credentials: HTTPAuthorizationCredentials = Depends(security)) -> JWTAccessToken:
+        token = credentials.credentials
+        try:
+            payload: JWTAccessToken = JWTAccessManager.decode_token(token)
+            if not payload.user_id or not payload.role:
+                raise HTTPException(status_code=401, detail="Неверные данные в токена")
+            return payload
+        except jwt.ExpiredSignatureError:
+            raise HTTPException(status_code=401, detail="Token expired (Access токен протух)")
+        except jwt.InvalidTokenError:
+            raise HTTPException(status_code=401, detail="Недействительный токен")
+
+
+class RoleChecker:
+    def __init__(self, allowed_roles: list[RoleEnum]):
+        self.allowed_roles = allowed_roles
+
+    def __call__(self, current_user: JWTAccessToken = Depends(AuthChecker())) -> JWTAccessToken:
+        if current_user.role not in self.allowed_roles:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="У вас недостаточно прав для выполнения этого действия"
+            )
+        return current_user
