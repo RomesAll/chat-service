2768e4c0bf0b27b736029c3ec545abd46d801191
RomesAll <romesky845@gmail.com>
2026-08-13 17:18:59 +0300

    feat: добавил репозиторий для пользователей в комнате UserInRoomRepository

--- app/data_access/database/repositories/room.py
+++ app/data_access/database/repositories/room.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
-from models.room import RoomOrm
+from dtos.room import UserInRoomDtoGetResponse
+from models.room import RoomOrm, UserInRoomOrm
 from .base import BaseRepository
 from app.shared.dtos import (
     BaseDtoClientRequest,
@@ -17,3 +18,12 @@ class RoomRepository(
         super().__init__(session=session)
         self.dto_response = RoomDtoGetResponse
         self.model = RoomOrm
+
+
+class UserInRoomRepository(
+    BaseRepository[BaseDtoClientRequest, RoomDtoGetResponse, RoomDtoPostRequest]
+):
+    def __init__(self, session: Session):
+        super().__init__(session=session)
+        self.dto_response = UserInRoomDtoGetResponse
+        self.model = UserInRoomOrm
