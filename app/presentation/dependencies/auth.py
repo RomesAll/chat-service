49459529bb90659f356d866ddb5c54466aa918a1
RomesAll <romesky845@gmail.com>
2026-08-20 14:37:01 +0300

    refactor: добавил в обработчик токенов возможность обрабатывать и refresh токены

--- app/presentation/dependencies/auth.py
+++ app/presentation/dependencies/auth.py
@@ -1,9 +1,10 @@
 from fastapi import Depends, HTTPException
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from starlette import status
-from business_logic.auth.jwt_manager import JWTAccessManager
-from dtos import JWTAccessToken
+from business_logic.auth.jwt_manager import JWTAccessManager, JWTRefreshManager
+from dtos import JWTBaseToken
 import jwt
+from dtos.jwt import TokenType, JWTAccessTokenResponse, JWTRefreshTokenResponse
 from models.user import RoleEnum
 
 security = HTTPBearer()
@@ -11,10 +12,20 @@ security = HTTPBearer()
 
 class AuthChecker:
     """Проверка существования access токена"""
-    def __call__(self, credentials: HTTPAuthorizationCredentials = Depends(security)) -> JWTAccessToken:
+    def __call__(
+            self,
+            credentials: HTTPAuthorizationCredentials = Depends(security),
+            token_type: TokenType = TokenType.ACCESS_TOKEN
+    ) -> JWTBaseToken:
         token = credentials.credentials
         try:
-            payload: JWTAccessToken = JWTAccessManager.decode_token(token)
+            payload = None
+            if token_type == TokenType.ACCESS_TOKEN:
+                payload: JWTAccessTokenResponse = JWTAccessManager.decode_token(token)
+            elif token_type == TokenType.REFRESH_TOKEN:
+                payload: JWTRefreshTokenResponse = JWTRefreshManager.decode_token(token)
+            if not payload:
+                raise Exception
             if not payload.user_id or not payload.role:
                 raise HTTPException(status_code=401, detail="Неверные данные в токена")
             return payload
@@ -29,7 +40,7 @@ class RoleChecker:
     def __init__(self, allowed_roles: list[RoleEnum]):
         self.allowed_roles = allowed_roles
 
-    def __call__(self, current_user: JWTAccessToken = Depends(AuthChecker())) -> JWTAccessToken:
+    def __call__(self, current_user: JWTBaseToken = Depends(AuthChecker())) -> JWTBaseToken:
         if current_user.role not in self.allowed_roles:
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
