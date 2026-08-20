bab7b6038a1011e9201243b7aaa3c07b5d6694cd
RomesAll <romesky845@gmail.com>
2026-08-20 15:00:50 +0300

    feat: use case для загрузки файлов с сервера

---
+++ app/business_logic/use_cases/message/download_file_use_case.py
@@ -0,0 +1,36 @@
+from uuid import UUID
+from starlette.responses import StreamingResponse
+
+from business_logic.exceptions import PermissionFileDownError
+from business_logic.file_manager.file_manager import FileManager
+from business_logic.unit_of_work import UnitOfWork
+from business_logic.use_cases.interface.iuse_case import IUseCase
+from repositories.message import PrivateMessageRepository
+from repositories.message_attachments import MessageAttachmentsRepository
+
+
+class DownloadFileUseCase(IUseCase):
+    """Use case для загрузки файла с сервера"""
+    def __init__(
+            self,
+            uow: UnitOfWork,
+            file_manager: type[FileManager]
+    ):
+        self.uow = uow
+        self.file_manager = file_manager
+
+    async def execute(self, user_upload_id: str, file_id: UUID) -> StreamingResponse:
+        with self.uow as uow:
+            file_repo = uow.get_repository(MessageAttachmentsRepository)
+            message_repo = uow.get_repository(PrivateMessageRepository)
+            file_meta = file_repo.get_by_id(file_id)
+            message_info = message_repo.get_by_id(file_meta.message_id)
+            if user_upload_id not in [message_info.sender_id, message_info.recipient_id]:
+                raise PermissionFileDownError(user_upload_id, file_id)
+            return StreamingResponse(
+                self.file_manager.read_file(file_meta.file_path),
+                media_type=file_meta.mime_type,
+                headers={
+                    "Content-Disposition": f"attachment; filename={file_meta.file_name}"
+                }
+            )
