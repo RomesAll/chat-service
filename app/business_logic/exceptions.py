2bf054889565e6cd19337a6ee689b89bb171c9c4
RomesAll <romesky845@gmail.com>
2026-08-20 14:49:25 +0300

    feat: долбавил исключения - UserNotFoundError пользователь не найден - CheckPswError неверный пароль - SyncKeyError ошибка синхронизации ключей - PermissionFileDownError нет доступа к файлу

--- app/business_logic/exceptions.py
+++ app/business_logic/exceptions.py
@@ -1,17 +1,16 @@
 from uuid import UUID
-from app.shared.dtos import BaseMessageDtoPostRequest
 
 
 class UserConnectionNotFound(Exception):
     """Не удачная попытка поиска подключений пользователя для отправки сообщения"""
-    def __init__(self, target_id: UUID):
+    def __init__(self, target_id: str):
         message = f'Подключения пользователя {target_id} не найдено'
         super().__init__(message)
 
 
 class SendMessageError(Exception):
     """Ошибка отправки сообщения"""
-    def __init__(self, sender_id: UUID, target_id: UUID, cause: str):
+    def __init__(self, sender_id: str, target_id: str, cause: str):
         message = (f'Не удалось отправить сообщение '
                         f'пользователю: {target_id} от {sender_id} '
                         f'по причине: {cause}')
@@ -20,7 +19,7 @@ class SendMessageError(Exception):
 
 class RouteMessageError(Exception):
     """Ошибка маршрутизации сообщения"""
-    def __init__(self, message_info: BaseMessageDtoPostRequest):
+    def __init__(self, message_info):
         message = f'Не удалось маршрутизировать сообщение {message_info}'
         super().__init__(message)
 
@@ -29,4 +28,32 @@ class ChatNotFound(Exception):
     """Комната не найдена"""
     def __init__(self, chat_id: UUID):
         message = f'Не удалось найти чат {chat_id} для broadcast'
+        super().__init__(message)
+
+
+class UserNotFoundError(Exception):
+    """Информация о пользователе не найдена"""
+    def __init__(self, user_id: str):
+        message = f'Информация о пользователе {user_id} не передана в метод для получения или создание активной сессии'
+        super().__init__(message)
+
+
+class CheckPswError(Exception):
+    """Переданный пароль не совпадает с сохраненным"""
+    def __init__(self, user_id: str):
+        message = f'Передан неверный пароль для пользователя {user_id}'
+        super().__init__(message)
+
+
+class SyncKeyError(Exception):
+    """Неудачная попытка синхронизации ключей между сессиями"""
+    def __init__(self, user_id: str):
+        message = f'Не удалось синхронизировать ключи для пользователя {user_id}'
+        super().__init__(message)
+
+
+class PermissionFileDownError(Exception):
+    """Нет прав для загрузки файлов"""
+    def __init__(self, user_id: str, file_id: UUID):
+        message = f'У пользователя {user_id} нет прав на доступ к файлу {file_id}'
         super().__init__(message)
