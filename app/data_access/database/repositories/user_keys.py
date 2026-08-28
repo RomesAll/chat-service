2a4325f3b2c1c8feac4edd7260a919907023cfec
RomesAll <romesky845@gmail.com>
2026-08-28 15:33:14 +0300

    refactor: обновил процесс сохранения ключа

--- app/data_access/database/repositories/user_keys.py
+++ app/data_access/database/repositories/user_keys.py
@@ -1,8 +1,8 @@
-from sqlalchemy import update, select, insert
+from sqlalchemy import update, select
 from sqlalchemy.sql.elements import and_
-from dtos.keys import PublicKeyDtoCreate, PublicKeyDtoGet, PrivateKeyDtoCreate, PrivateKeyDtoGet
+from app.shared.dtos.keys import PublicKeyDtoCreate, PublicKeyDtoGet, PrivateKeyDtoCreate, PrivateKeyDtoGet
 from app.data_access.database.models import UserPublicKey, UserPrivateKey
-from exceptions import RecordNotFound
+from app.data_access.exceptions import RecordNotFound
 from .base import BaseRepositoryGet, BaseRepositorySave
 
 
@@ -56,29 +56,23 @@ class PrivateKeyRepository(
     model: type[UserPrivateKey] = UserPrivateKey
     dto_response: type[PrivateKeyDtoGet] = PrivateKeyDtoGet
 
-    def get_by_id(self, user_id: str) -> PrivateKeyDtoGet:
+    def get_by_id(self, user_id: str) -> list[PrivateKeyDtoGet]:
         """Получение зашифрованной коллекции приватных ключей пользователя по id пользователя"""
         stmt = (
             select(self.model).
             where(self.model.user_id == user_id)
         )
-        result: UserPrivateKey | None = self.session.execute(stmt).scalar_one_or_none()
-        if not result:
+        results = self.session.execute(stmt).scalars().all()
+        if not results:
             raise RecordNotFound(user_id)
-        return self.dto_response(**result.to_dict())
+        return [self.dto_response(**res.to_dict()) for res in results]
 
-    def save(self, key_info: PrivateKeyDtoCreate) -> None:
+    def save(self, key_info: PrivateKeyDtoCreate) -> list[PrivateKeyDtoGet]:
         """Сохранение зашифрованной коллекции приватных ключей пользователя"""
-        is_exists = self.check_exist(key_info.user_id)
-        if not is_exists:
-            stmt_update_or_insert = (
-                insert(self.model).
-                values(user_id=key_info.user_id, encrypted_private_keys=key_info.encrypted_private_keys)
-            )
-        else:
-            stmt_update_or_insert = (
-                update(self.model).
-                where(self.model.user_id == key_info.user_id).
-                values(encrypted_private_keys=key_info.encrypted_private_keys)
-            )
-        self.session.execute(stmt_update_or_insert)
+        orm_object = self.model(**key_info.model_dump())
+        self.session.add(orm_object)
+        self.session.flush()
+        stmt_get_private_keys = select(self.model).where(self.model.user_id == key_info.user_id)
+        results = list(self.session.execute(stmt_get_private_keys).scalars().all())
+        results.append(orm_object)
+        return [self.dto_response(**res.to_dict()) for res in results]
