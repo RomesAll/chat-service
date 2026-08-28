192e4fa6bda79bd8f95bfb78a4d469a4885b8ed4
RomesAll <romesky845@gmail.com>
2026-08-28 15:30:02 +0300

    feat: добавил api для пол. груп. сообщения, удаления и отправки

--- app/presentation/rest_api/messages/messages.py
+++ app/presentation/rest_api/messages/messages.py
@@ -1,26 +1,31 @@
+import json
 from typing import Annotated
-from uuid import UUID
+from uuid import UUID, uuid4
 from fastapi import APIRouter, status, Depends, File, UploadFile as FastAPIUploadFile, Form
 from pydantic import WithJsonSchema
 from starlette.responses import JSONResponse
-from business_logic.active_session.active_session_manager import active_session_manager
-from business_logic.active_session.message_sender.private_message import PrivateMessageRoute
-from app.business_logic.cache.session_key_storage import session_key_storage
-from business_logic.encryption.symmetric import SymmetricEncode
-from business_logic.file_manager.file_manager import FileManager
-from business_logic.unit_of_work import UnitOfWork
-from business_logic.use_cases.message.download_file_use_case import DownloadFileUseCase
-from business_logic.use_cases.message.send_private_msg_and_save_use_case import SendPrivateMsgAndSave
-from database import db
-from dtos import (
+from bootstrap import get_bootstrap
+from app.business_logic.active_session.message_sender.group_message import GroupMessageRoute
+from app.business_logic.active_session.message_sender.private_message import PrivateMessageRoute
+from app.business_logic.encryption.symmetric import SymmetricEncode
+from app.business_logic.file_manager.file_manager import FileManager
+from app.business_logic.unit_of_work import UnitOfWork
+from app.business_logic.use_cases.message.delete_msg_use_case import DeleteMsgUseCase
+from app.business_logic.use_cases.message.download_file_use_case import DownloadFileUseCase
+from app.business_logic.use_cases.message.get_messages_use_case import GetPrivateMsgAndSave
+from app.business_logic.use_cases.message.send_group_msg_and_save_use_case import SendGroupMsgAndSave
+from app.business_logic.use_cases.message.send_private_msg_and_save_use_case import SendPrivateMsgAndSave
+from app.shared.dtos import (
     PrivateMessageDtoPostRequest,
     JWTAccessToken,
+    GroupMessageDtoPostRequest, MessageType,
 )
-from models.user import RoleEnum
-from presentation.dependencies.auth import RoleChecker
+from app.data_access.database.models.user import RoleEnum
+from app.presentation.dependencies.auth import RoleChecker
 
 
 route = APIRouter()
+bootstrap = get_bootstrap()
 
 UploadFile = Annotated[
     FastAPIUploadFile, WithJsonSchema({"type": "string", "format": "binary"})
@@ -28,20 +33,21 @@ UploadFile = Annotated[
 
 
 @route.post(
-    path='/message/send',
+    path='/users/{recipient_id}/message/send',
+    #path='/message/send',
     tags=['Message'],
     summary='Отправка сообщения пользователю',
     description='Отправка сообщения пользователю',
     operation_id='send_private_message_operation'
 )
 async def send_private_message(
+        recipient_id: str,
         message_id: UUID = Form(...),
-        recipient_id: str = Form(...),
         message_for_recipient: str | None = Form(None),
-        recipient_key_version: str = Form(...),
+        recipient_key_version: str = Form(None),
         sender_id: str = Form(...),
         message_for_sender: str | None = Form(None),
-        sender_key_version: str = Form(...),
+        sender_key_version: str = Form(None),
         files: list[UploadFile] | None = File(None),
         access_token: JWTAccessToken = Depends(RoleChecker([RoleEnum.DEFAULT_USER, RoleEnum.SUPER_ADMIN]))
 ):
@@ -71,12 +77,12 @@ async def send_private_message(
         )
     private_msg_response = await SendPrivateMsgAndSave(
         session_id=access_token.session_id,
-        uow=UnitOfWork(db),
+        uow=UnitOfWork(bootstrap.database),
         file_manager=FileManager,
         private_msg_route=PrivateMessageRoute(
-            active_session=active_session_manager,
+            active_session=bootstrap.active_session_manager,
             symmetric_encode=SymmetricEncode,
-            session_key_storage=session_key_storage
+            session_key_storage=bootstrap.session_key_storage
         )
     ).execute(
         dto_private_msg=dto_request,
@@ -100,10 +106,105 @@ async def download_file(
         access_token: JWTAccessToken = Depends(RoleChecker([RoleEnum.DEFAULT_USER, RoleEnum.SUPER_ADMIN]))
 ):
     result = await DownloadFileUseCase(
-        uow=UnitOfWork(db),
+        uow=UnitOfWork(bootstrap.database),
         file_manager=FileManager
     ).execute(
         user_upload_id=access_token.user_id,
         file_id=file_id
     )
     return result
+
+
+@route.get(
+    path='/users/me/message/history/{user_id}',
+    #path='/message/me/history/{user_id}',
+    tags=['Message'],
+    summary='Получение истории сообщений с пользователем',
+    operation_id='get_history_message'
+)
+def get_message(
+        user_id: str,
+        access_token: JWTAccessToken = Depends(RoleChecker([RoleEnum.DEFAULT_USER, RoleEnum.SUPER_ADMIN]))
+):
+    results = GetPrivateMsgAndSave(
+        uow=UnitOfWork(bootstrap.database)
+    ).execute(
+        user_id_who=access_token.user_id,
+        user_id_whom=user_id
+    )
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content=[result.model_dump(mode='json') for result in results]
+    )
+
+
+@route.post(
+    path='/rooms/{room_id}/message/send',
+    #path='/message/group/send',
+    tags=['Room', 'Message'],
+    summary='Отправка сообщения в группу',
+    operation_id='send_group_message_operation'
+)
+async def send_group_message(
+        room_id: UUID,
+        sender_id: str = Form(...),
+        payload: str = Form(None),
+        files: list[UploadFile] | None = File(None),
+        access_token: JWTAccessToken = Depends(RoleChecker([RoleEnum.DEFAULT_USER, RoleEnum.SUPER_ADMIN]))
+):
+    dto_request = GroupMessageDtoPostRequest(
+        id=uuid4(),
+        room_id=room_id,
+        sender_id=sender_id,
+        payload=json.loads(payload)
+    )
+    if not dto_request.payload['message'] and not files:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content='Попытка отправить пустое сообщение'
+        )
+    if dto_request.sender_id != access_token.user_id:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content='id отправителя не совпадает с id пользователя в токене'
+        )
+    result = await SendGroupMsgAndSave(
+        session_id=access_token.session_id,
+        file_manager=FileManager,
+        uow=UnitOfWork(bootstrap.database),
+        group_msg_route=GroupMessageRoute(bootstrap.active_session_manager),
+    ).execute(
+        dto_group_msg=dto_request,
+        upload_file=files
+    )
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content=result.model_dump(mode='json')
+    )
+
+
+@route.delete(
+    path='/messages/{message_id}/delete',
+    tags=['Message'],
+    summary='Удаление сообщения',
+    operation_id='delete_msg_operaion'
+)
+async def delete_message(
+    message_id: UUID,
+    type_message: MessageType,
+    access_token: JWTAccessToken = Depends(RoleChecker([RoleEnum.DEFAULT_USER, RoleEnum.SUPER_ADMIN]))
+):
+    await DeleteMsgUseCase(
+        uow=UnitOfWork(bootstrap.database),
+        file_manager=FileManager,
+        active_session_manager=bootstrap.active_session_manager,
+        type_message=type_message
+    ).execute(
+        sender_id=access_token.user_id,
+        message_id=message_id,
+        type_msg=type_message
+    )
+    return JSONResponse(
+        status_code=status.HTTP_204_NO_CONTENT,
+        content=f'Сообщение с id {message_id} удалено'
+    )
