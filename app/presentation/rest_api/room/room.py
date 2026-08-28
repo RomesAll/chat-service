7a097fc4a151c6c73e977ab71d83c71966aca206
RomesAll <romesky845@gmail.com>
2026-08-28 15:31:39 +0300

    feat: добавил api для комнат

---
+++ app/presentation/rest_api/room/room.py
@@ -0,0 +1,83 @@
+from uuid import UUID
+from fastapi import APIRouter, status, Depends
+from starlette.responses import JSONResponse
+from bootstrap import get_bootstrap
+from app.business_logic.unit_of_work import UnitOfWork
+from app.business_logic.use_cases.room.get_pub_key_user_in_room_use_case import GetPubKeyUserInRoomUseCase
+from app.business_logic.use_cases.room.save_room_use_case import SaveRoomUseCase
+from app.business_logic.use_cases.room.save_user_in_room_use_case import SaveUserInRoomUseCase
+from app.shared.dtos import (
+    JWTAccessToken,
+    RoomDtoPostRequest,
+    UserInRoomPostRequest,
+)
+from app.data_access.database.models.user import RoleEnum
+from app.presentation.dependencies.auth import RoleChecker
+
+
+route = APIRouter()
+bootstrap = get_bootstrap()
+
+@route.post(
+    path='/rooms/new-room',
+    tags=['Room'],
+    summary='Создание комнаты',
+    operation_id='save_room_operation'
+)
+def save_new_room(
+        dto_request: RoomDtoPostRequest,
+        access_token: JWTAccessToken = Depends(RoleChecker([RoleEnum.DEFAULT_USER, RoleEnum.SUPER_ADMIN]))
+):
+    result = SaveRoomUseCase(
+        uow=UnitOfWork(bootstrap.database),
+    ).execute(
+        dto_request=dto_request,
+    )
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content=result.model_dump(mode='json')
+    )
+
+
+@route.post(
+    path='/rooms/{room_id}/users/{user_id}/add',
+    tags=['Room'],
+    summary='Добавление нового пользователя в комнату',
+    operation_id='add_user_in_room_operation'
+)
+def add_user_in_room(
+        user_id: str,
+        room_id: UUID,
+        access_token: JWTAccessToken = Depends(RoleChecker([RoleEnum.DEFAULT_USER, RoleEnum.SUPER_ADMIN]))
+):
+    result = SaveUserInRoomUseCase(
+        uow=UnitOfWork(bootstrap.database),
+    ).execute(
+        dto_request=UserInRoomPostRequest(
+            room_id=room_id,
+            user_id=user_id
+        ),
+    )
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content=result.model_dump(mode='json')
+    )
+
+
+@route.get(
+    path='/room/{room_id}/user-public-key',
+    tags=['Room'],
+    summary='Получение всех публичных ключей пользователей в комнате',
+    operation_id='get_pub_key_operation'
+)
+def get_pub_key_users_in_room(
+        room_id: UUID,
+        access_token: JWTAccessToken = Depends(RoleChecker([RoleEnum.DEFAULT_USER, RoleEnum.SUPER_ADMIN]))
+):
+    results = GetPubKeyUserInRoomUseCase(
+        uow=UnitOfWork(bootstrap.database),
+    ).execute(room_id=room_id)
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content=results
+    )
