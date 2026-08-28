4c113074268b3e534e664e1e0710efd69dbd5ef9
RomesAll <romesky845@gmail.com>
2026-08-28 15:27:59 +0300

    feat: импортироывал все ручки, установил конфигурацию проекта

--- main.py
+++ main.py
@@ -1,40 +1,39 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from bootstrap import set_bootstrap
+from app.shared.config import DevelopConfig
 
-from app.shared.dtos.base import BaseDtoCreateUpdateDeleteWithReturnValueRequest, BaseDtoGetListRequest, \
-    PaginationDto, SortDto, SortEnum, FilterDto, OperatorEnum
-from app.shared.dtos.room import RoomDtoGetResponse
-from models.metaclasses import AutoCreateTable
-# from app.business_logic.dtos.base import (
-#     DtoGetByIdRequest,
-#     BaseDtoGetRequest,
-#     BaseDtoPostRequest,
-#     BaseDtoUpdateRequest,
-#     BaseDtoDeleteRequest,
-#     BaseDtoGetResponse,
-# )
-from repositories.base import BaseRepository
+config = DevelopConfig()
+set_bootstrap(config)
 
-engine = create_engine(url='sqlite:///test.db')
-session_factory = sessionmaker(bind=engine)
-AutoCreateTable.set_engine(engine)
+from starlette.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from app.presentation.rest_api.websocket.websocket import route as ws_route
+from app.presentation.rest_api.messages.messages import route as msg_route
+from app.presentation.rest_api.users.get_users import route as get_users_route
+from app.presentation.rest_api.room.room import route as save_user_in_room_route
+from app.presentation.rest_api.users.update_users import route as update_users_route
+from app.presentation.rest_api.users.delete_users import route as delete_users_route
+from app.presentation.rest_api.keys.keys import route as keys_route
+from app.presentation.rest_api.auth.auth import route as auth_route
+from app.presentation.rest_api.db.database import route as db_route
+import uvicorn
 
-from models.room import RoomOrm
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(ws_route)
+app.include_router(msg_route, prefix='/api/v1')
+app.include_router(get_users_route, prefix='/api/v1')
+app.include_router(update_users_route, prefix='/api/v1')
+app.include_router(delete_users_route, prefix='/api/v1')
+app.include_router(auth_route, prefix='/api/v1')
+app.include_router(keys_route, prefix='/api/v1')
+app.include_router(save_user_in_room_route, prefix='/api/v1')
+app.include_router(db_route, prefix='/api/v1')
 
-with session_factory() as session:
-    repository = BaseRepository(
-        dto_response=RoomDtoGetResponse,
-        session=session,
-        model=RoomOrm
-    )
-    dto_request: BaseDtoGetListRequest = BaseDtoGetListRequest(
-        pagination=PaginationDto(limit=1, offset=0),
-        include_deleted=True,
-        sort=[SortDto(field='name', order_mode=SortEnum.DESC), SortDto(field='id', order_mode=SortEnum.DESC)],
-        filters=[FilterDto(field='name', operator=OperatorEnum.EQ, value='aboba'), FilterDto(field='name', operator=OperatorEnum.EQ, value='bab')],
-        filters_logic='OR'
-    )
-    result = repository.get(dto_request)
-    session.commit()
-
-print(result)
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
