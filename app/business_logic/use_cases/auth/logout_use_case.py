fd8980167c3486c72e4365281537432bd2a70742
RomesAll <romesky845@gmail.com>
2026-09-05 12:51:08 +0300

    refactor: сделал уделение инф. о токенов и сессионного ключа через синтаксис python словаря

--- app/business_logic/use_cases/auth/logout_use_case.py
+++ app/business_logic/use_cases/auth/logout_use_case.py
@@ -17,13 +17,7 @@ class LogoutUseCase(IUseCase):
         self.white_list = white_list
         self.session_key_storage = session_key_storage
 
-    def execute(self, user_id: str, refresh_token_id: UUID, session_id: UUID) -> tuple[bool, bool]:
-        is_delete_refresh = self.white_list.delete_refresh_token(
-            user_id=user_id,
-            token_id=refresh_token_id,
-        )
-        is_delete_session_key = self.session_key_storage.delete(
-            user_id=user_id,
-            session_id=session_id,
-        )
-        return is_delete_refresh, is_delete_session_key
+    def execute(self, user_id: str, refresh_token_id: UUID, session_id: UUID):
+        del self.white_list[user_id, refresh_token_id]
+        del self.session_key_storage[user_id, session_id]
+        self.active_session_manager.remove_user_active_session(user_id, session_id)
