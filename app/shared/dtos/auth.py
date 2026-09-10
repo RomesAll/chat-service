3d6c4d258e239f2b1560a8e67793ac9e6bc60dd2
RomesAll <romesky845@gmail.com>
2026-09-10 15:55:09 +0300

    feat: добавил dto для работы с кодами подтверждения

--- app/shared/dtos/auth.py
+++ app/shared/dtos/auth.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel, SecretStr
-from .user import UserDtoGetResponse
-from .jwt import JWTTokenResponse
+from datetime import datetime, timezone
+from enum import Enum
+from pydantic import BaseModel, SecretStr, Field
 
 
 class LoginDtoRequest(BaseModel):
@@ -9,6 +9,25 @@ class LoginDtoRequest(BaseModel):
     password: SecretStr
 
 
-class LoginOrRegisterDtoResponse(JWTTokenResponse):
+class SendType(str, Enum):
+    PHONE = 'телефон'
+    EMAIL = 'email'
+
+
+class LoginOrRegisterDtoResponse(BaseModel):
     """DTO для получения информации о пользователе после входа или регистрации"""
-    user_info: UserDtoGetResponse
+    send_code: SendType
+    time_send: datetime = Field(default_factory=lambda : datetime.now(tz=timezone.utc))
+
+
+class VerifyCodeRequest(BaseModel):
+    """DTO для передачи и проверки кода подтверждения"""
+    user_id: str
+    email: str
+    code: int
+
+
+class RefreshVerifyCodeRequest(BaseModel):
+    """DTO для обновления кода"""
+    user_id: str
+    email: str
