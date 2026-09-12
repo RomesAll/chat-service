3f2c5e8a19a547caa03dc8001dba526cf0978d5b
RomesAll <romesky845@gmail.com>
2026-09-12 17:59:48 +0300

    fix: передал во вложенную функцию в декораторе именнованные аргументы, т.к. без них они не передавались

--- app/business_logic/decorators.py
+++ app/business_logic/decorators.py
@@ -1,5 +1,6 @@
 import logging
 from functools import wraps
+from typing import Self
 from app.shared.dtos import AuditPostDto
 from bootstrap import get_bootstrap
 from app.shared.config import config
@@ -9,10 +10,10 @@ logger = logging.getLogger(config.log_info.log_name)
 def audit_system(func):
     """Декоратор для добавления данных в ауди-сервис"""
     @wraps(func)
-    def wrapper(self, **kwargs):
+    def wrapper(self: Self, *args, **kwargs):
         dto_audit: AuditPostDto | None = self.__dict__.get('dto_audit', None)
         try:
-            result = func(self, **kwargs)
+            result = func(self, *args, **kwargs)
             if dto_audit:
                 dto_audit.result = result
             return result
