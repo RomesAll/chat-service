6fd2a75c4425b7f08b5ac6cbad32b318fb4bf8fb
RomesAll <romesky845@gmail.com>
2026-09-12 18:01:57 +0300

    refactor: явно передал все аргументы при создании orm модели, т.к. были проблемы с сериализации SecretStr у pydantic атрибута

--- app/data_access/database/repositories/user.py
+++ app/data_access/database/repositories/user.py
@@ -1,17 +1,17 @@
 from sqlalchemy import select
 from app.data_access.exceptions import RecordNotFound
-from app.data_access.database.models.user import UserOrm
+from app.data_access.database.models.user import UserOrm, RoleEnum
 from .base import BaseRepositoryGet, BaseRepositorySave, BaseRepositoryDelete, BaseRepositoryUpdate
 from app.shared.dtos import (
-    UserDtoGetResponse,
-    UserDtoPostRequest,
+    UserDtoGetResponse, UserDtoPostRequestWithRole,
+
 )
 
 
 class UserRepository(
     BaseRepositoryGet[UserDtoGetResponse, UserOrm],
-    BaseRepositorySave[UserDtoGetResponse, UserDtoPostRequest, UserOrm],
-    BaseRepositoryUpdate[UserDtoGetResponse, UserDtoPostRequest, UserOrm],
+    BaseRepositorySave[UserDtoGetResponse, UserDtoPostRequestWithRole, UserOrm],
+    BaseRepositoryUpdate[UserDtoGetResponse, UserDtoPostRequestWithRole, UserOrm],
     BaseRepositoryDelete[UserDtoGetResponse, UserOrm]
 ):
     """Репозиторий для работы с данными пользователей"""
@@ -24,10 +24,16 @@ class UserRepository(
         user_info = self.session.execute(stmt).scalar_one()
         return self._get_dto(user_info)
 
-    def save(self, dto_post_request: UserDtoPostRequest) -> UserDtoGetResponse:
+    def save(self, dto_post_request: UserDtoPostRequestWithRole) -> UserDtoGetResponse:
         """Сохранения пользователя"""
-        orm_object = self.model(**dto_post_request.model_dump())
-        orm_object.password = dto_post_request.password.get_secret_value().encode()
+        orm_object = self.model(
+            id=dto_post_request.id,
+            email=dto_post_request.email,
+            phone=dto_post_request.phone,
+            user_name=dto_post_request.user_name,
+            password=dto_post_request.password.get_secret_value().encode(),
+            role=dto_post_request.role,
+        )
         self.session.add(orm_object)
         self.session.flush()
         return self._get_dto(orm_object)
@@ -39,3 +45,13 @@ class UserRepository(
         if not password:
             raise RecordNotFound(user_id)
         return password
+
+    def update_role(self, user_id: str, role: RoleEnum) -> UserDtoGetResponse:
+        """Изменение роли у пользователя"""
+        stmt = select(self.model).where(self.model.id == user_id)
+        user = self.session.execute(stmt).scalar_one_or_none()
+        if not user:
+            raise RecordNotFound(user_id)
+        user.role = role
+        self.session.flush()
+        return self._get_dto(user)
