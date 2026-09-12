7cb58649070290474ee5de50963d2f9f8516f64d
RomesAll <romesky845@gmail.com>
2026-09-12 18:04:50 +0300

    fix: переместил api с путем, где содержится me выше по иерархии для api удаления пользователя

--- app/presentation/rest_api/users/delete_users.py
+++ app/presentation/rest_api/users/delete_users.py
@@ -14,34 +14,6 @@ bootstrap = get_bootstrap()
 
 
 @route.patch(
-    path='/users/{user_id}/soft-delete',
-    tags=['Users'],
-    summary='Мягкое удаление пользователя',
-    description='Мягкое удаление с возможность восстановления',
-    response_model_exclude_unset=True,
-    response_model=UserDtoGetResponse,
-    operation_id="soft_delete_operation",
-)
-def soft_delete_user(
-        user_id: str,
-        request_client_dep: RequestClientDtoHandle = Depends(
-            RequestClientDepends[JWTAccessToken](
-                allowed_roles=[RoleEnum.SUPER_ADMIN],
-                action_type=ActionType.SOFT_DELETE_USER
-            )
-        )
-):
-    result: str = SoftDeleteUser(
-        uow=UnitOfWork(bootstrap.database),
-        dto_audit=request_client_dep.dto_audit
-    ).execute(user_id)
-    return JSONResponse(
-        status_code=status.HTTP_200_OK,
-        content=f'Пользователь с id {result} теперь неактивен'
-    )
-
-
-@route.patch(
     path='/users/me/soft-delete',
     tags=['Users'],
     summary='Мягкое удаление своей информации по токену',
@@ -68,29 +40,31 @@ def soft_delete_me(
     )
 
 
-@route.delete(
-    path='/users/{user_id}/hard-delete',
+@route.patch(
+    path='/users/{user_id}/soft-delete',
     tags=['Users'],
-    summary='Жесткое удаление пользователя',
-    description='Жесткое удаление без возможности восстановления',
-    operation_id="hard_delete_operation",
+    summary='Мягкое удаление пользователя',
+    description='Мягкое удаление с возможность восстановления',
+    response_model_exclude_unset=True,
+    response_model=UserDtoGetResponse,
+    operation_id="soft_delete_operation",
 )
-def hard_delete_user(
+def soft_delete_user(
         user_id: str,
         request_client_dep: RequestClientDtoHandle = Depends(
             RequestClientDepends[JWTAccessToken](
                 allowed_roles=[RoleEnum.SUPER_ADMIN],
-                action_type=ActionType.HARD_DELETE_USER
+                action_type=ActionType.SOFT_DELETE_USER
             )
         )
 ):
-    result: str = HardDeleteUser(
+    result: str = SoftDeleteUser(
         uow=UnitOfWork(bootstrap.database),
         dto_audit=request_client_dep.dto_audit
     ).execute(user_id)
     return JSONResponse(
         status_code=status.HTTP_200_OK,
-        content=f'Пользователь с id {result} теперь удален'
+        content=f'Пользователь с id {result} теперь неактивен'
     )
 
 
@@ -119,35 +93,29 @@ def hard_delete_me(
     )
 
 
-@route.post(
-    path='/users/{user_id}/recovery',
+@route.delete(
+    path='/users/{user_id}/hard-delete',
     tags=['Users'],
-    summary='Восстановление пользователя',
-    description='Восстановление пользователя после мягкого удаления',
-    operation_id="recovery_operation",
+    summary='Жесткое удаление пользователя',
+    description='Жесткое удаление без возможности восстановления',
+    operation_id="hard_delete_operation",
 )
-def recovery_user(
+def hard_delete_user(
         user_id: str,
-        return_record: bool = True,
         request_client_dep: RequestClientDtoHandle = Depends(
             RequestClientDepends[JWTAccessToken](
                 allowed_roles=[RoleEnum.SUPER_ADMIN],
-                action_type=ActionType.RECOVERY_USER
+                action_type=ActionType.HARD_DELETE_USER
             )
         )
 ):
-    result: UserDtoGetResponse = RecoveryUser(
+    result: str = HardDeleteUser(
         uow=UnitOfWork(bootstrap.database),
         dto_audit=request_client_dep.dto_audit
     ).execute(user_id)
-    if not return_record:
-        return Response(
-            status_code=status.HTTP_204_NO_CONTENT
-        )
     return JSONResponse(
-        status_code=status.HTTP_201_CREATED,
-        content=result.model_dump(mode='json'),
-        media_type="application/json"
+        status_code=status.HTTP_200_OK,
+        content=f'Пользователь с id {result} теперь удален'
     )
 
 
@@ -180,5 +148,37 @@ def recovery_me(
         content={
             'message': result.model_dump(mode='json')
         },
+        media_type="application/json"
+    )
+
+
+@route.post(
+    path='/users/{user_id}/recovery',
+    tags=['Users'],
+    summary='Восстановление пользователя',
+    description='Восстановление пользователя после мягкого удаления',
+    operation_id="recovery_operation",
+)
+def recovery_user(
+        user_id: str,
+        return_record: bool = True,
+        request_client_dep: RequestClientDtoHandle = Depends(
+            RequestClientDepends[JWTAccessToken](
+                allowed_roles=[RoleEnum.SUPER_ADMIN],
+                action_type=ActionType.RECOVERY_USER
+            )
+        )
+):
+    result: UserDtoGetResponse = RecoveryUser(
+        uow=UnitOfWork(bootstrap.database),
+        dto_audit=request_client_dep.dto_audit
+    ).execute(user_id)
+    if not return_record:
+        return Response(
+            status_code=status.HTTP_204_NO_CONTENT
+        )
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content=result.model_dump(mode='json'),
         media_type="application/json"
     )
