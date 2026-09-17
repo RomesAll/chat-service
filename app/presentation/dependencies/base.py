eda57fd4ec4a791c4e6eff83509394bfb30bcde6
RomesAll <romesky845@gmail.com>
2026-09-17 13:43:01 +0300

    refactor: добавил параметр тип токена, чтобы проверять верный ли токен передан при использовании api

--- app/presentation/dependencies/base.py
+++ app/presentation/dependencies/base.py
@@ -5,15 +5,18 @@ from app.data_access.database.models.user import RoleEnum
 from app.shared.dtos.base import TToken
 from .audit import AuditDep
 from .auth import AuthChecker
+from ...shared.dtos.jwt import TokenType
 
 
 class RequestClientDepends(Generic[TToken]):
     """Базовая зависимость DI для fastapi end-point"""
     def __init__(
             self,
+            token_type: TokenType,
             allowed_roles: list[RoleEnum],
             action_type: ActionType
     ):
+        self.token_type = token_type
         self.allowed_roles = allowed_roles
         self.action_type = action_type
 
@@ -26,6 +29,11 @@ class RequestClientDepends(Generic[TToken]):
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
                 detail="У вас недостаточно прав для выполнения этого действия"
+            )
+        if token_info.type != self.token_type:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Передан неверный тип токена для выполнения дествия"
             )
         dto_audit.user_id = token_info.user_id
         dto_audit.action = self.action_type
