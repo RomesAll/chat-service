482688cf19625e486c4b5eaa442885c857c660b2
RomesAll <romesky845@gmail.com>
2026-09-17 13:49:33 +0300

    feat: добавил асинхронных декоратор для аудит системы для асинхронных функций

--- app/business_logic/decorators.py
+++ app/business_logic/decorators.py
@@ -1,17 +1,17 @@
 import logging
 from functools import wraps
-from typing import Self
 from app.shared.dtos import AuditPostDto
 from bootstrap import get_bootstrap
 from app.shared.config import config
 
 logger = logging.getLogger(config.log_info.log_name)
 
+
 def audit_system(func):
-    """Декоратор для добавления данных в ауди-сервис"""
+    """Декоратор для синхронных функций"""
     @wraps(func)
-    def wrapper(self: Self, *args, **kwargs):
-        dto_audit: AuditPostDto | None = self.__dict__.get('dto_audit', None)
+    def wrapper(self, *args, **kwargs):
+        dto_audit = self.__dict__.get('dto_audit')
         try:
             result = func(self, *args, **kwargs)
             if dto_audit:
@@ -26,6 +26,28 @@ def audit_system(func):
             if dto_audit:
                 _save_audit(dto_audit)
     return wrapper
+
+
+def audit_system_async(func):
+    """Декоратор для асинхронных функций"""
+    @wraps(func)
+    async def wrapper(self, *args, **kwargs):
+        dto_audit = self.__dict__.get('dto_audit')
+        try:
+            result = await func(self, *args, **kwargs)
+            if dto_audit:
+                dto_audit.result = result
+            return result
+        except Exception as e:
+            if dto_audit:
+                dto_audit.successfully = False
+                dto_audit.exceptions = str(e)
+            raise
+        finally:
+            if dto_audit:
+                _save_audit(dto_audit)
+    return wrapper
+
 
 def _save_audit(dto_audit: AuditPostDto):
     try:
